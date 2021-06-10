6e7161af0486aa308701c0ae222a7c1547b9c061
dalu <moutong945@outlook.com>
2021-06-10 14:55:33 +0800

    add package header

--- sinolor-themes.el
+++ sinolor-themes.el
@@ -1,5 +1,27 @@
 ;;; sinolor-themes.el --- an opinionated pack of modern color-themes -*- lexical-binding: t; -*-
 
+;; Author: dalu <mou.tong@qq.com>
+;; Maintainer: dalu <mou.tong@qq.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1") (cl-lib "0.5"))
+;; Homepage: https://github.com/dalugm/sinolor-themes
+;; Keywords: themes, faces
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; Sinolor Themes is an opinionated UI plugin and pack of themes
