d7cb93e8a8b3e3d39e270c51d28d0a2715e987fd
dalu <moutong945@outlook.com>
2022-06-22 04:02:51 +0800

    feat: make nobreak-space visible

--- sinolor-themes-base.el
+++ sinolor-themes-base.el
@@ -29,7 +29,7 @@
      (&light :background (sinolor-themes--blend bg highlight 0.7) :foreground base0 :distant-foreground base8))
     (match                :foreground green      :background base0 :weight 'bold)
     (trailing-whitespace  :background red)
-    (nobreak-space        :inherit 'default :underline nil)
+    (nobreak-space        :inherit 'escape-glyph :underline t)
     (vertical-border      :background vertical-bar :foreground vertical-bar)
     (link                 :foreground highlight :underline t :weight 'bold)
     (error   :foreground error)
